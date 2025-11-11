bb265382c664e40ee0c3cc5feba7f06c0f03a411
sreeman <sreemanlingala333@gmail.com>
2025-11-11 23:41:47 +0530

    initial ommits

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+
+const a:string="sreeman"
+console.log(a + "is hot")
+const app = express();
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send("hello world")
+})
+function test<T>(){
+   let a = 5
+   const p=new Promise((res,rej)=>{
+        setTimeout(()=>{a=10;res(a)},2000);
+
+   }
+
+     )
+  return p
+}
+app.listen(3000, () => {
+    console.log("server is running")
+    test.length
+    
+
+})
